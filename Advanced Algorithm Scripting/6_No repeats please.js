af5170c2d62d417ea475356ccfbdce41848e137c
JFMalan <jacques.malan26@gmail.com>
2017-07-10 12:24:16 +0200

    Update 6_No repeats please.js

--- Advanced Algorithm Scripting/6_No repeats please.js
+++ Advanced Algorithm Scripting/6_No repeats please.js
@@ -1,6 +1,38 @@
 
 function permAlone(str) {
-  return str;
+  
+  var arr = str.split('');
+  var result = 0;
+  var ar2 =[];
+  //make swap func
+  function swap(a,b){
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  } 
+  //heaps algirithom
+  function generate(n){
+    //regex to filter out dubbels
+    var regex = /([a-z])\1+/;
+    if(n===1 && !regex.test(arr.join(''))){
+       result++;
+       }
+    else{
+      for(var i = 0; i < n; i++){
+        generate(n-1);
+        //if n even
+        if(n % 2 ===0){
+        swap(i,n-1);
+        }
+        //if n odd
+        else{
+          swap(0,n-1);
+        }
+      }
+    }    
+  }
+  generate(str.length);
+  return result;
 }
 
 permAlone('aab');
